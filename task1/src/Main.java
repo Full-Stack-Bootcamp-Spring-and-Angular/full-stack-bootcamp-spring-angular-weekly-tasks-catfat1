b79753c612bfc1fa6eafd1beab1c712239ba6db9
Heba Taha <enghebatahaiti34@gmail.com>
2025-12-17 19:39:22 +0200

    task1 solution

---
+++ task1/src/Main.java
@@ -0,0 +1,30 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext container=new ClassPathXmlApplicationContext("applicationContext.xml");
+        Scanner scanner=new Scanner(System.in);
+
+        Circle circle=(Circle) container.getBean("myCircle");
+        Square square=(Square) container.getBean("mySquare");
+
+        System.out.println("Enter Circle Radius: ");
+        double radius=scanner.nextDouble();
+        circle.setRadius(radius);
+        double circleArea=circle.getArea();
+        System.out.println("the area is :" +circleArea);
+
+
+        System.out.println("Enter Square Side: ");
+        double side=scanner.nextDouble();
+        square.setSide(side);
+        double squareArea= square.getArea();
+        System.out.println("the area is :" +squareArea);
+
+
+
+    }
+}
